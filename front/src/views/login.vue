5cdc8d7f0d9e0b452fbba2715f7eb54aa1b03e25
Chlorine001 <1151259938@qq.com>
2026-03-16 19:32:21 +0800

    登录查询数据库数据，而不是测试数据--按钮权限问题待优化

--- front/src/views/login.vue
+++ front/src/views/login.vue
@@ -24,12 +24,12 @@
                   <template #prepend><span class="sign">密码：</span></template>
                 </el-input>
               </div>
-              <div>
-                <el-input placeholder="输入验证码" value="dragon"
-                          style="background-color: rgba(255, 255, 255, .7);border-radius: 40px;">
-                  <template #prepend><span class="sign">验证码：</span></template>
-                </el-input>
-              </div>
+<!--              <div>-->
+<!--                <el-input placeholder="输入验证码" value="dragon"-->
+<!--                          style="background-color: rgba(255, 255, 255, .7);border-radius: 40px;">-->
+<!--                  <template #prepend><span class="sign">验证码：</span></template>-->
+<!--                </el-input>-->
+<!--              </div>-->
             </div>
             <div style="padding:20px 40px 0; text-align: center;">
               <span style="display: inline-block;width: 70%;max-width: 150px;">
@@ -38,16 +38,16 @@
                     type="primary" :loading="loading" size="large" @click="tologin">登录</el-button>
               </span>
             </div>
-            <div style="padding: 20px 30px;">
-              <div
-                  style="position: relative; padding-top:20px; text-align: center;color: black;overflow: hidden;">
-                教师身份：除admin外，任意登录<br>管理员：账号admin，密码123456
+<!--            <div style="padding: 20px 30px;">-->
+<!--              <div-->
+<!--                  style="position: relative; padding-top:20px; text-align: center;color: black;overflow: hidden;">-->
+<!--                教师身份：除admin外，任意登录<br>管理员：账号admin，密码123456-->
                 <!--                <div-->
                 <!--                    style="position: absolute;top:-1px;left:-1px; padding-top:20px; width: 100%; text-align: center;color: white;">-->
                 <!--                  管理员：admin账号，123456密码，游客身份：除了admin之外，随便输-->
-                <!--                </div>-->
-              </div>
-            </div>
+<!--                                </div>-->
+<!--              </div>-->
+<!--            </div>-->
           </div>
         </div>
       </div>
@@ -60,168 +60,46 @@
 <script>
 import {fns, getglobalbtn} from '../utils.js'
 import {ElMessage} from 'element-plus'
+import { http } from '@/api/http'
+import { loadMenuRoutes, addMenuRoutes } from '@/router'
 
-var btnroot = ['edit', 'del', 'add']//假设这是按钮的权限配置
 export default {
   data() {
     return {
       logined: false,
-      routes: [//假设这是后端给的数据
-        {
-          path: '/home/home',
-          root: [btnroot[0], btnroot[1]]
-        },
-        //上面那个必须要有
-        {
-          path: '/home/systemSet',
-          root: []
-        },
-        {
-          path: '/home/systemSet/routes',
-          root: []
-        },
-        {
-          path: '/home/systemSet/dictsset',
-          root: []
-        },
-        {
-          path: '/home/systemSet/department',
-          root: []
-        },
-        {
-          path: '/home/systemSet/page1',
-          root: [btnroot[0]],
-          dicts: ['page1:edit:ban', 'page1:edit:userroot'],//字典相关
-        },
-        {
-          path: '/home/systemSet/userset',
-          root: [btnroot[0]],
-        },
-        {
-          path: '/home/systemSet/pagerootsettip',
-          root: [btnroot[0]],
-          dicts: ['pagerootsettip:view:test', 'pagerootsettip:edit:test'],//字典相关
-        },
-
-        {
-          path: '/home/page1b',
-          root: []
-        },
-        {
-          path: '/home/page1b/subpage1b',
-          root: []
-        },
-        {
-          path: '/home/page1b2',
-          root: []
-        },
-        {
-          path: '/home/page1b2/subpage1b2',
-          root: []
-        },
-        {
-          path: '/home/page1b2/subpage1b2/subpage1b2s',
-          root: []
-        },
-        {
-          path: '/home/plugins',
-          root: []
-        },
-        {
-          path: '/home/icon',
-          root: btnroot
-        },
-        {
-          path: '/home/mobilepageview',
-          root: btnroot
-        },
-        {
-          path: '/home/setting',
-          root: btnroot
-        },
-        {
-          path: '/home/personal',
-          root: btnroot
-        }
-      ],
-      routes2: [//假设这是后端给的数据
-        {
-          path: '/home/home',
-          root: [btnroot[0], btnroot[1]]
-        },
-        //上面那个必须要有
-        {
-          path: '/home/page1b',
-          root: []
-        },
-        {
-          path: '/home/page1b/subpage1b',
-          root: []
-        },
-        {
-          path: '/home/page1b2',
-          root: []
-        },
-        {
-          path: '/home/page1b2/subpage1b2',
-          root: []
-        },
-        {
-          path: '/home/page1b2/subpage1b2/subpage1b2s',
-          root: []
-        },
-        {
-          path: '/home/else_demo_list',
-          root: btnroot
-        },
-        {
-          path: '/home/setting',
-          root: btnroot
-        },
-
-        {
-          path: '/home/personal',
-          root: btnroot
-        }
-      ],
       account: 'admin',
-      pass: 123456,
+      pass: 'admin',
       loading: false,
     }
   },
   methods: {
-    tologin() {
-      this.loading = true;
-      setTimeout(() => {
-        this.loading = false;
-        this.logined = true
-        localStorage.setItem('token', 'en12039120938109203')
-        if (this.account == 'admin') {
-          if (this.pass != 123456) {
-            ElMessage({type: 'error', message: '密码错误'})
-            return
-          }
-          localStorage.setItem('navroot', JSON.stringify(this.routes))
-          this.$router.options.routes = fns(this.$router, this.routes).getRoutes()
-          setTimeout(() => {
-            if ((localStorage.getItem('navroot') || []).length == 0) {
-              ElMessage({type: 'error', message: '没有获取到页面地址组'})
-            } else {
-              this.$router.replace('/home/home')
-            }
-          }, 200)
-        } else {
-          localStorage.setItem('navroot', JSON.stringify(this.routes2))
-          this.$router.options.routes = fns(this.$router, this.routes2).getRoutes()
-          setTimeout(() => {
-            if ((localStorage.getItem('navroot') || []).length == 0) {
-              ElMessage({type: 'error', message: '没有获取到页面地址组'})
-            } else {
-              this.$router.replace('/home/home')
-            }
-          }, 200)
+    async tologin() {
+      this.loading = true
+      try {
+        const res = await http.postJson('/auth/login', { account: this.account, pass: this.pass }, { timeoutMs: 5000 })
+        if (!res || res.ok !== true) {
+          ElMessage({ type: 'error', message: (res && res.message) ? res.message : '登录失败' })
+          return
         }
-      }, 1000)
+        this.logined = true
+        localStorage.setItem('token', res.token || 'token')
+
+        const navroot = Array.isArray(res.navroot) ? res.navroot : []
+        localStorage.setItem('navroot', JSON.stringify(navroot))
+        this.$router.options.routes = fns(this.$router, navroot).getRoutes()
+
+        setTimeout(() => {
+          if ((localStorage.getItem('navroot') || []).length === 0) {
+            ElMessage({ type: 'error', message: '没有获取到页面地址组' })
+          } else {
+            this.$router.replace('/home/home')
+          }
+        }, 200)
+      } catch (e) {
+        ElMessage({ type: 'error', message: e?.message === 'timeout' ? '登录超时，请检查后端服务！' : '登录失败，请检查后端服务！' })
+      } finally {
+        this.loading = false
+      }
     }
   }
 }
