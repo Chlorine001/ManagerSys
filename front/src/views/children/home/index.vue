16884b0f306994b6ebeb9addf55e1d6612ba26e5
Chlorine001 <1151259938@qq.com>
2026-03-17 18:27:09 +0800

    feat(home): 更新首页和个人页面样式与功能
    - 为首页日历组件添加自定义样式，包括边框颜色、鼠标悬停效果和选中状态样式
    - 在个人页面集成用户信息获取功能，动态显示用户名
    - 优化代码注释和布局结构
    - 在退出登录时增加清除用户信息的逻辑
    - 调整响应式断点样式格式，提高代码可读性

--- front/src/views/children/home/index.vue
+++ front/src/views/children/home/index.vue
@@ -1,48 +1,44 @@
 <template>
-    <div>
-<!--        <div style="margin-bottom: 20px;padding:10px; background-color: white;" :style="{ boxShadow: $root.boxShadow }">-->
-<!--            <span style="color:#999">-->
-<!--                这个项目目前全程都是以localStorage本地缓存为主要的缓存形式，实际开发请酌情处理-->
-<!--            </span>-->
-<!--        </div>-->
-        <div class="leftshow1 a">
-            <div class="leftshow1in" :style="{ boxShadow: $root.boxShadow }">
-                <homeCount />
-            </div>
-        </div>
-        <div class="leftshow2">
-            <div class="leftshow2in" :style="{ boxShadow: $root.boxShadow }">
-                <homeHeadvue />
-            </div>
-        </div>
-        <div class="leftshow1 b">
-            <div class="leftshow1in" :style="{ boxShadow: $root.boxShadow }">
-                <homeCount />
-            </div>
-        </div>
-        <!--  -->
-        <div class="leftshow1">
-            <div class="leftshow1in" style="min-height: 449px;" :style="{ boxShadow: $root.boxShadow }">
-                <el-calendar v-model="value" />
-            </div>
-        </div>
-        <div class="leftshow2">
-            <div class="leftshow2in" :style="{ boxShadow: $root.boxShadow }">
-                <radarEchart ref="radarEchart" />
-            </div>
-        </div>
-        <div class="leftshow1">
-            <div class="leftshow1in" :style="{ boxShadow: $root.boxShadow }">
-                <barEchart ref="barEchart" />
-            </div>
-        </div>
-        <div class="leftshow2">
-            <div class="leftshow2in" :style="{ boxShadow: $root.boxShadow }">
-                <pieEchart ref="pieEchart" />
-            </div>
-        </div>
-        <div style="clear: both;"></div>
+  <div>
+    <!--      首页上部-->
+    <div class="leftshow1 a">
+      <div class="leftshow1in" :style="{ boxShadow: $root.boxShadow }">
+        <homeCount/>
+      </div>
     </div>
+    <div class="leftshow2">
+      <div class="leftshow2in" :style="{ boxShadow: $root.boxShadow }">
+        <homeHeadvue/>
+      </div>
+    </div>
+    <div class="leftshow1 b">
+      <div class="leftshow1in" :style="{ boxShadow: $root.boxShadow }">
+        <homeCount/>
+      </div>
+    </div>
+    <!--      日期和图表-->
+    <div class="leftshow1">
+      <div class="leftshow1in" style="min-height: 449px;" :style="{ boxShadow: $root.boxShadow }">
+        <el-calendar class="el-calendar-table" v-model="value"/>
+      </div>
+    </div>
+    <div class="leftshow2">
+      <div class="leftshow2in" :style="{ boxShadow: $root.boxShadow }">
+        <radarEchart ref="radarEchart"/>
+      </div>
+    </div>
+    <div class="leftshow1">
+      <div class="leftshow1in" :style="{ boxShadow: $root.boxShadow }">
+        <barEchart ref="barEchart"/>
+      </div>
+    </div>
+    <div class="leftshow2">
+      <div class="leftshow2in" :style="{ boxShadow: $root.boxShadow }">
+        <pieEchart ref="pieEchart"/>
+      </div>
+    </div>
+    <div style="clear: both;"></div>
+  </div>
 </template>
 
 <script>
@@ -51,96 +47,118 @@ import homeHeadvue from '../components/home_headview.vue'
 import radarEchart from '../components/echarts_radar.vue'
 import barEchart from '../components/echarts_bar.vue'
 import pieEchart from '../components/echarts_pie.vue'
+
 export default {
-    data() {
-        return {
-            value: new Date()
-        }
-    },
-    components: {
-        homeCount,
-        homeHeadvue,
-        radarEchart,
-        barEchart,
-        pieEchart
-    },
-    methods: {
-        setresize() {
-            this.$refs.radarEchart && this.$refs.radarEchart.resizeEchart()
-            this.$refs.barEchart && this.$refs.barEchart.resizeEchart()
-            this.$refs.pieEchart && this.$refs.pieEchart.resizeEchart()
-        }
-    },
-    watch: {
-        '$root.openchange': function (news, olds) {
-            this.$nextTick(() => {
-                this.setresize()
-            })
-        }
-    },
-    mounted() {
-        window.addEventListener('resize', () => {
-            this.setresize()
-        })
+  data() {
+    return {
+      value: new Date()
     }
+  },
+  components: {
+    homeCount,
+    homeHeadvue,
+    radarEchart,
+    barEchart,
+    pieEchart
+  },
+  methods: {
+    setresize() {
+      this.$refs.radarEchart && this.$refs.radarEchart.resizeEchart()
+      this.$refs.barEchart && this.$refs.barEchart.resizeEchart()
+      this.$refs.pieEchart && this.$refs.pieEchart.resizeEchart()
+    }
+  },
+  watch: {
+    '$root.openchange': function (news, olds) {
+      this.$nextTick(() => {
+        this.setresize()
+      })
+    }
+  },
+  mounted() {
+    window.addEventListener('resize', () => {
+      this.setresize()
+    })
+  }
 }
 </script>
 
 <style scoped>
 .leftshow1,
 .leftshow2 {
-    float: left;
-    margin-bottom: 20px;
+  float: left;
+  margin-bottom: 20px;
 }
 
 .leftshow1in,
 .leftshow2in {
-    background: white;
+  background: white;
 }
 
-@media (min-width:1066px) {
-    .b {
-        display: none;
-    }
-
-    .leftshow1 {
-        width: 60%;
-    }
-
-    .leftshow2 {
-        width: 40%;
-    }
-
-    .leftshow1in {
-        margin-right: 20px;
-    }
-
-    .leftshow1in,
-    .leftshow2in {
-        min-height: 100px;
-    }
+/* 日历边框背景颜色 */
+:deep(.el-calendar-table td, .el-calendar-table tr:first-child td, .el-calendar-table tr td:first-child) {
+  border-color: black !important;
 }
 
-@media (max-width:1065px) {
-    .a {
-        display: none;
-    }
+/*  日历单元格鼠标滑过背景色 */
+:deep(.el-calendar-table .el-calendar-day:hover) {
+  background-color: lightblue !important;
+}
 
-    .leftshow1 {
-        width: 100%;
-    }
+:deep(.el-calendar-table .is-today) {
+  color: red !important;
+  background-color: white;
+}
 
-    .leftshow2 {
-        width: 100%;
-    }
+:deep(.el-calendar-table .is-selected ) {
+  color: black;
+  background-color: lightcyan !important;
+}
 
-    .leftshow1in {
-        margin-right: 0px;
-    }
 
-    .leftshow1in,
-    .leftshow2in {
-        min-height: 100px;
-    }
+@media (min-width: 1066px) {
+  .b {
+    display: none;
+  }
+
+  .leftshow1 {
+    width: 60%;
+  }
+
+  .leftshow2 {
+    width: 40%;
+  }
+
+  .leftshow1in {
+    margin-right: 20px;
+  }
+
+  .leftshow1in,
+  .leftshow2in {
+    min-height: 100px;
+  }
+}
+
+@media (max-width: 1065px) {
+  .a {
+    display: none;
+  }
+
+  .leftshow1 {
+    width: 100%;
+  }
+
+  .leftshow2 {
+    width: 100%;
+  }
+
+  .leftshow1in {
+    margin-right: 0px;
+  }
+
+  .leftshow1in,
+  .leftshow2in {
+    min-height: 100px;
+  }
 }
 </style>
--- front/src/views/children/personal/index.vue
+++ front/src/views/children/personal/index.vue
@@ -1,92 +1,103 @@
 <template>
-    <div>
-        <div style="margin-bottom: 20px;padding: 0 0 20px; background-color: white;"
-            :style="{ boxShadow: $root.boxShadow }">
-            <personalHeadvue />
-        </div>
-        <div style="margin-bottom: 20px;padding:30px 20px; background-color: white;"
-            :style="{ boxShadow: $root.boxShadow }">
-            <div style="display: flex;margin-bottom: 20px;">
-                <div><span style="display: inline-block;width:100px">用户名称：</span> </div>
-                <el-input v-if="type == 1" value="Admin"></el-input>
-                <div v-else>Admin</div>
-            </div>
-            <div style="display: flex;margin-bottom: 20px;">
-                <div><span style="display: inline-block;width:100px">生日：</span> </div>
-                <el-input v-if="type == 1" value="2020-10-20"></el-input>
-                <div v-else>2020-10-20</div>
-            </div>
-            <div style="display: flex;margin-bottom: 20px;">
-                <div><span style="display: inline-block;width:100px">用户类型：</span> </div>
-                <el-select v-if="type == 1" style="width:100%" v-model="userrootselect">
-                  <el-option :value="0" label="超级管理员">超级管理员</el-option>
-                  <el-option :value="1" label="学生用户">学生用户</el-option>
-                  <el-option :value="2" label="教师用户">教师用户</el-option>
-                </el-select>
-                <div v-else>学生用户</div>
-            </div>
-            <div style="display: flex;margin-bottom: 20px;">
-                <div><span style="display: inline-block;width:100px">单项：</span> </div>
-                <div v-if="type == 1">
-                    <el-radio-group v-model="radio1" class="ml-4">
-                        <el-radio label="1">是</el-radio>
-                        <el-radio label="2">否</el-radio>
-                    </el-radio-group>
-                </div>
-                <div v-else>是</div>
-            </div>
-            <div style="display: flex;margin-bottom: 20px;">
-                <div><span style="display: inline-block;width:100px">多项：</span> </div>
-                <div v-if="type == 1">
-                    <el-checkbox v-model="checked1" label="Option 1" size="large" />
-                    <el-checkbox v-model="checked2" label="Option 2" size="large" />
-                    <el-checkbox v-model="checked3[0]" label="Option 3" size="large" />
-                </div>
-                <div v-else>选项1，选项2</div>
-            </div>
-            <div style="display: flex;margin-bottom: 20px;">
-                <div><span style="display: inline-block;width:100px"></span> </div>
-                <div>
-                    <el-button v-if="type == 0" @click="type = 1">修改</el-button>
-                    <el-button v-if="type == 1" @click="type = 0">完成</el-button>
-                </div>
-            </div>
-        </div>
-        <el-button @click="loginout">退出登录</el-button>
+  <div>
+    <div style="margin-bottom: 20px;padding: 0 0 20px; background-color: white;"
+         :style="{ boxShadow: $root.boxShadow }">
+      <personalHeadvue/>
     </div>
+    <div style="margin-bottom: 20px;padding:30px 20px; background-color: white;"
+         :style="{ boxShadow: $root.boxShadow }">
+      <div style="display: flex;margin-bottom: 20px;">
+        <div><span style="display: inline-block;width:100px">用户名称：</span></div>
+        <el-input v-if="type == 1" :value="currentUser?.name || 'Admin'"></el-input>
+        <div v-else>{{ currentUser?.name || 'Admin' }}</div>
+      </div>
+      <div style="display: flex;margin-bottom: 20px;">
+        <div><span style="display: inline-block;width:100px">生日：</span></div>
+        <el-input v-if="type == 1" value="2020-10-20"></el-input>
+        <div v-else>2020-10-20</div>
+      </div>
+      <div style="display: flex;margin-bottom: 20px;">
+        <div><span style="display: inline-block;width:100px">用户类型：</span></div>
+        <el-select v-if="type == 1" style="width:100%" v-model="userrootselect">
+          <el-option :value="0" label="超级管理员">超级管理员</el-option>
+          <el-option :value="1" label="学生用户">学生用户</el-option>
+          <el-option :value="2" label="教师用户">教师用户</el-option>
+        </el-select>
+        <div v-else>学生用户</div>
+      </div>
+      <div style="display: flex;margin-bottom: 20px;">
+        <div><span style="display: inline-block;width:100px">单项：</span></div>
+        <div v-if="type == 1">
+          <el-radio-group v-model="radio1" class="ml-4">
+            <el-radio label="1">是</el-radio>
+            <el-radio label="2">否</el-radio>
+          </el-radio-group>
+        </div>
+        <div v-else>是</div>
+      </div>
+      <div style="display: flex;margin-bottom: 20px;">
+        <div><span style="display: inline-block;width:100px">多项：</span></div>
+        <div v-if="type == 1">
+          <el-checkbox v-model="checked1" label="Option 1" size="large"/>
+          <el-checkbox v-model="checked2" label="Option 2" size="large"/>
+          <el-checkbox v-model="checked3[0]" label="Option 3" size="large"/>
+        </div>
+        <div v-else>选项1，选项2</div>
+      </div>
+      <div style="display: flex;margin-bottom: 20px;">
+        <div><span style="display: inline-block;width:100px"></span></div>
+        <div>
+          <el-button v-if="type == 0" @click="type = 1">修改</el-button>
+          <el-button v-if="type == 1" @click="type = 0">完成</el-button>
+        </div>
+      </div>
+    </div>
+    <el-button @click="loginout">退出登录</el-button>
+  </div>
 </template>
 
 <script>
 import personalHeadvue from '../components/personal_headview.vue'
+import {getCurrentUser, getCurrentUserName} from '@/utils/user'
+
 export default {
-    data() {
-        return {
-            type: 0,//0为查看，1为编辑
-            //
-            userrootselect: 1,
-            radio1: '1',
-            checked1: true,
-            checked2: false,
-            checked3: [true],
-        }
-    },
-    methods: {
-        loginout() {
-            var confirms = confirm('确定退出登录？')
-            if (confirms) {
-                //token过期也按如下方式处理清除信息，不建议直接clear全部本地缓存清除
-                localStorage.removeItem('token')
-                localStorage.removeItem('navroot')
-                //这一步非常重要
-                setTimeout(() => {
-                    location.reload(location.href.split('#')[0] + '#/')
-                    // location.reload('/')//非hash模式
-                })
-            }
-        }
-    },
-    components: {
-        personalHeadvue
+  data() {
+    return {
+      type: 0,//0 为查看，1 为编辑
+      currentUser: null,
+      //
+      userrootselect: 1,
+      radio1: '1',
+      checked1: true,
+      checked2: false,
+      checked3: [true],
     }
+  },
+  mounted() {
+    // 获取当前用户信息
+    this.currentUser = getCurrentUser()
+    console.log('个人资料页面 - 当前用户:', this.currentUser)
+  },
+  methods: {
+    getCurrentUser,
+    getCurrentUserName,
+    loginout() {
+      var confirms = confirm('确定退出登录？')
+      if (confirms) {
+        //token 过期也按如下方式处理清除信息，不建议直接 clear 全部本地缓存清除
+        localStorage.removeItem('token')
+        localStorage.removeItem('navroot')
+        localStorage.removeItem('currentUser')// 清除用户信息
+        //这一步非常重要
+        setTimeout(() => {
+          location.reload(location.href.split('#')[0] + '#/')
+          // location.reload('/')//非 hash 模式
+        })
+      }
+    }
+  },
+  components: {
+    personalHeadvue
+  }
 }
 </script>
